a8dfbf446b8706d8b1d42a9bc242d98ff2bc449c
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-28 16:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const hamburger = document.getElementById('hamburger');
 const navSub = document.querySelector('.nav-sub');
 
 hamburger.addEventListener('click', () => {
-  if (navSub.style.display === 'flex') {
-    navSub.style.display = 'none';
-  } else {
-    navSub.style.display = 'flex';
-  }
+  navSub.classList.toggle('active');
 });
