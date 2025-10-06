e905a67c8e002797ce3a17895eacd6caf2381d03
Ishika Verma <ishikaverma2317@gmail.com>
2025-10-06 06:52:18 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navSub = document.querySelector('.nav-sub');
-
-hamburger.addEventListener('click', () => {
-  navSub.classList.toggle('active');
-});
